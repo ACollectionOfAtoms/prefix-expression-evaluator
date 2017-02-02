1086985cf4464f81ff23278b588927e0a538ebdf
Adam <davidatomhernandez@gmail.com>
2017-02-02 16:20:51 -0500

    updating readme, gitignore, argparser

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from argparse import ArgumentParser
 import os.path
 
 parser = ArgumentParser(description='Process text files with prefix expressions per newline')
-parser.add_argument('-f', dest='filename', required=False, metavar="FILE")
+parser.add_argument('-file', dest='filename', required=False, metavar="FILE")
 
 filename = parser.parse_args().filename
 pre_eval = PrefixEvaluator()
